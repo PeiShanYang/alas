2ecc8bda289ca659b70de3033ae2264cbdc3668d
JoeJHHuang <joe.jh.huang@auo.com>
2022-09-02 09:48:06 +0000

    set enter enterModel as function

--- enterModel.py
+++ enterModel.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-
-"""
-Created on WED MAR 24 17:00:00 2022
-@author: ShanYang
-"""
-
 from main.Run import run_process
 
-if __name__ == "__main__":
+def enterModel():
    run_process()
--- so/enter.py
+++ so/enter.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-
-"""
-Created on WED MAR 24 17:00:00 2022
-@author: ShanYang
-"""
-
 from main.Main import main
 
-if __name__ == "__main__":
+def enter():
     main()
